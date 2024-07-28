0be09f81ac3e11a3b78c0d988bd351d5285a0cbf
jaceleedev <jaceleedev@gmail.com>
2024-07-28 19:23:24 +0900

    feat: :sparkles: add HeroContent component

---
+++ src/blocks/HeroSection/HeroContent/index.tsx
@@ -0,0 +1,21 @@
+import Button from '@/components/Button';
+
+function HeroContent() {
+  return (
+    <div className="self-end">
+      <h1 className="text-heading-1 text-center mb-[34px]">
+        Group Chat for Everyone
+      </h1>
+      <p className="text-body text-center mb-8">
+        Meet makes it easy to connect with others face-to-face virtually and
+        collaborate across any device.
+      </p>
+      <div className="flex justify-center gap-4">
+        <Button variant="primary" primaryText="Download" secondaryText="v1.3" />
+        <Button variant="secondary" primaryText="What is it?" />
+      </div>
+    </div>
+  );
+}
+
+export default HeroContent;
