8290a82d16dcea0f47508967c8bbe02ffc106a02
jaceleedev <jaceleedev@gmail.com>
2024-07-29 21:34:54 +0900

    feat: :sparkles: implement NumberedCircle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from '@/blocks/HeroSection';
+import NumberedCircle from '@/components/NumberedCircle';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <div className="flex justify-center mt-[110px] mb-16">
+        <NumberedCircle number="01" />
+      </div>
     </main>
   );
 }
---
+++ src/components/NumberedCircle/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+interface NumberedCircleProps {
+  number: string;
+}
+
+function NumberedCircle({ number }: Readonly<NumberedCircleProps>) {
+  return (
+    <div className="flex flex-col items-center">
+      <Image src="/images/vertical-line.svg" alt="" width={1} height={84} />
+      <div className="flex justify-center items-center text-numbered-circle w-14 h-14 border border-solid border-[rgba(135,135,157,0.25)] rounded-full bg-neutral-light">
+        {number}
+      </div>
+    </div>
+  );
+}
+
+export default NumberedCircle;
