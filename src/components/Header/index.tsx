bde6b7dbf141592d7591f7bdfa1703da59709967
jaceleedev <jaceleedev@gmail.com>
2024-07-30 21:21:17 +0900

    feat: :iphone: add responsive layout for mobile in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 function Header() {
   return (
-    <header className="flex justify-center w-full pt-20 pb-[61px] max-lg:pt-12 max-lg:pb-16">
+    <header className="flex justify-center w-full pt-20 pb-[61px] max-lg:pt-12 max-lg:pb-16 max-sm:pb-[54px]">
       <Image
         src="/images/logo.svg"
         alt="Meet logo"
