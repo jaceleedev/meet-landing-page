30ec125a05fd692a9e67d3f86d2230158e2eca78
jaceleedev <jaceleedev@gmail.com>
2024-07-30 14:15:23 +0900

    refactor: :recycle: implement ContentDivisionSection for consistent page segmentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
+import ContentDivisionSection from '@/blocks/ContentDivisionSection';
 import FeaturesSection from '@/blocks/FeaturesSection';
 import HeroSection from '@/blocks/HeroSection';
 import UseCasesSection from '@/blocks/UseCasesSection';
-import NumberedCircle from '@/components/NumberedCircle';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <div className="flex justify-center mt-[110px] mb-16">
-        <NumberedCircle number="01" />
-      </div>
+      <ContentDivisionSection />
       <UseCasesSection />
       <FeaturesSection />
     </main>
---
+++ src/blocks/ContentDivisionSection/index.tsx
@@ -0,0 +1,11 @@
+import NumberedCircle from '@/components/NumberedCircle';
+
+function ContentDivisionSection() {
+  return (
+    <section className="flex justify-center mt-[110px] mb-16">
+      <NumberedCircle number="01" />
+    </section>
+  );
+}
+
+export default ContentDivisionSection;
