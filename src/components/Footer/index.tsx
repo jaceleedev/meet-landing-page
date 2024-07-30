0f59074b57f4b6cb111a88bcd8ee1ad7fceebcb4
jaceleedev <jaceleedev@gmail.com>
2024-07-30 23:22:56 +0900

    feat: :iphone: implement responsive layout for mobile in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,17 +18,24 @@ function Footer() {
           alt="Footer background"
           fill
           priority
-          className="object-cover opacity-[0.1045] hidden max-lg:block"
+          className="object-cover opacity-[0.1045] hidden max-lg:block max-sm:hidden"
+        />
+        <Image
+          src="/images/mobile/image-footer.jpg"
+          alt="Footer background"
+          fill
+          priority
+          className="object-cover opacity-[0.1045] hidden max-sm:block"
         />
       </div>
       <div className="absolute left-1/2 top-[calc(-140px+28px)] transform -translate-x-1/2">
         <NumberedCircle number="02" />
       </div>
-      <div className="relative z-10 grid grid-cols-[auto_auto_auto] max-w-[1110px] mx-auto max-lg:grid-cols-1 max-lg:grid-rows-[auto_auto_auto] max-lg:max-w-[573px] max-lg:justify-items-center">
-        <h2 className="text-heading-2 text-neutral-light mr-[125px] max-lg:mr-0 max-lg:mb-8 max-lg:max-w-[457px] max-lg:text-center">
+      <div className="relative z-10 grid grid-cols-[auto_auto_auto] max-w-[1110px] mx-auto max-lg:grid-cols-1 max-lg:grid-rows-[auto_auto_auto] max-lg:max-w-[573px] max-lg:justify-items-center max-sm:max-w-[327px]">
+        <h2 className="text-heading-2 text-neutral-light mr-[125px] max-lg:mr-0 max-lg:mb-8 max-lg:max-w-[457px] max-lg:text-center max-sm:text-[32px] max-sm:leading-[36px] max-sm:mb-6">
           Experience more together
         </h2>
-        <p className="text-body text-neutral-light mr-[92px] max-lg:mr-0 max-lg:mb-10 max-lg:text-center">
+        <p className="text-body text-neutral-light mr-[92px] max-lg:mr-0 max-lg:mb-10 max-lg:text-center max-sm:mb-8">
           Stay connected with reliable HD meetings and unlimited one-on-one and
           group video sessions.
         </p>
