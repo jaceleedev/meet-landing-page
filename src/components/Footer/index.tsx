8ddcd00893165294e980e81c6e5657a7638b0aa9
jaceleedev <jaceleedev@gmail.com>
2024-07-30 21:13:48 +0900

    feat: :iphone: add responsive layout for tablet in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,24 +4,31 @@ import Button from '../Button';
 
 function Footer() {
   return (
-    <footer className="relative h-[308px] pt-[116px] mt-[calc(140px-28px)]">
+    <footer className="relative h-[308px] pt-[116px] mt-[calc(140px-28px)] max-lg:h-[428px] max-lg:pt-[92px]">
       <div className="absolute inset-0 z-0 bg-primary-teal">
         <Image
           src="/images/desktop/image-footer.jpg"
           alt="Footer background"
           fill
           priority
-          className="object-cover opacity-[0.1045]"
+          className="object-cover opacity-[0.1045] max-lg:hidden"
+        />
+        <Image
+          src="/images/tablet/image-footer.jpg"
+          alt="Footer background"
+          fill
+          priority
+          className="object-cover opacity-[0.1045] hidden max-lg:block"
         />
       </div>
       <div className="absolute left-1/2 top-[calc(-140px+28px)] transform -translate-x-1/2">
         <NumberedCircle number="02" />
       </div>
-      <div className="relative z-10 grid grid-cols-[auto_auto_auto] max-w-[1110px] mx-auto">
-        <h2 className="text-heading-2 text-neutral-light mr-[125px]">
+      <div className="relative z-10 grid grid-cols-[auto_auto_auto] max-w-[1110px] mx-auto max-lg:grid-cols-1 max-lg:grid-rows-[auto_auto_auto] max-lg:max-w-[573px] max-lg:justify-items-center">
+        <h2 className="text-heading-2 text-neutral-light mr-[125px] max-lg:mr-0 max-lg:mb-8 max-lg:max-w-[457px] max-lg:text-center">
           Experience more together
         </h2>
-        <p className="text-body text-neutral-light mr-[92px]">
+        <p className="text-body text-neutral-light mr-[92px] max-lg:mr-0 max-lg:mb-10 max-lg:text-center">
           Stay connected with reliable HD meetings and unlimited one-on-one and
           group video sessions.
         </p>
