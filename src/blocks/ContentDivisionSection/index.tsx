faee507fa2c548de4f2012b40c75a0b0ee4479dd
jaceleedev <jaceleedev@gmail.com>
2024-07-30 23:00:19 +0900

    feat: :iphone: add responsive layout for mobile in ContentDivisionSection

--- src/blocks/ContentDivisionSection/index.tsx
+++ src/blocks/ContentDivisionSection/index.tsx
@@ -2,7 +2,7 @@ import NumberedCircle from '@/components/NumberedCircle';
 
 function ContentDivisionSection() {
   return (
-    <section className="flex justify-center pt-[110px] pb-16 max-lg:pt-20 max-lg:pb-[67px]">
+    <section className="flex justify-center pt-[110px] pb-16 max-lg:pt-20 max-lg:pb-[67px] max-sm:py-16">
       <NumberedCircle number="01" />
     </section>
   );
