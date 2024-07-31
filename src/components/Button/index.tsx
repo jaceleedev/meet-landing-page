c481a6c820e34f9199278087eb63745644b40d9f
jaceleedev <jaceleedev@gmail.com>
2024-07-31 22:38:24 +0900

    refactor: :recycle: optimize button size logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,7 +16,7 @@ function Button({
   onClick,
 }: Readonly<ButtonProps>) {
   const baseStyle =
-    'flex justify-center items-center gap-1 text-button rounded-[29px]';
+    'flex justify-center items-center gap-1 h-[58px] text-button rounded-[29px]';
   const variantStyle = {
     primary: 'bg-primary-teal hover:bg-[#71C0D4]',
     secondary: 'bg-secondary-purple hover:bg-[#B18BDD]',
@@ -25,8 +25,12 @@ function Button({
     variant === 'primary'
       ? 'text-accent-light-blue'
       : 'text-accent-light-purple';
-  const paddingStyle = secondaryText ? 'px-10 py-4' : 'px-[29px] py-4';
-  const widthStyle = secondaryText ? 'w-[193px]' : 'w-[139px]';
+
+  const getSizeStyle = (hasSecondaryText: boolean) => {
+    return hasSecondaryText
+      ? 'px-10 py-4 w-[193px]'
+      : 'px-[29px] py-4 w-[139px]';
+  };
 
   function handleClick() {
     if (onClick) {
@@ -39,7 +43,9 @@ function Button({
   return (
     <button
       onClick={handleClick}
-      className={`${baseStyle} ${variantStyle[variant]} ${paddingStyle} ${widthStyle}`}
+      className={`${baseStyle} ${variantStyle[variant]} ${getSizeStyle(
+        !!secondaryText
+      )}`}
     >
       <span className="text-white">{primaryText}</span>
       {secondaryText && (
